47172ffc61ddcaaabb7d4ad83bf1dbd18af8cd02
glp.gokulpatil <glp.gokulpatil@gmail.com>
2022-05-24 08:36:45 +0530

    commit with reverse code

--- c1.py
+++ c1.py
@@ -1,1 +1,13 @@
-print("welcome")
+
+#write a program to find the reverse of given integer number
+
+no=int(input("Enter the number"))
+def rev_no(num):
+    rev=0
+    while num!=0:
+        rev=rev*10+num%10
+        num=num//10
+    return rev
+
+
+print("The reversd of the given number",rev_no(no))
